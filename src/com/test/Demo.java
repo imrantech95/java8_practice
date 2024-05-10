b9331932e56722771e7c650f45962db29cfd49a3
unknown <ashuimran999@gmail.com>
2024-05-10 19:06:33 +0530

    added if condition java_8_practice demo local code repo

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -4,6 +4,11 @@ public class Demo {
 
 	public void addition(String data) {
 		System.out.println("String Data: "+data);
+		if(data.equals("Imran")) {
+			System.out.println("Welcome");
+		}else {
+			System.out.println("wrong");
+		}
 	}
 	
 	public static void main(String[] args) {
